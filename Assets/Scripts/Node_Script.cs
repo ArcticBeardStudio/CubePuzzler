d13c4fae068413be485a3f94acf94f5707bd30a6
Mathias Litzén <matlit-2@student.ltu.se>
2018-01-12 23:24:13 +0100

    First Commit
    First Commit

---
+++ Assets/Scripts/Node_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node_Script : MonoBehaviour {
+
+    //VARIABLES
+    // NodeType - 0 = goalNode, 1 = pathNode, 2 = noPathNode, 3 = startNode, 4 = endNode
+    // ColorType - 0 = Green, 1 = Blue, 2 = Red, 3 = Yellow, 4 = Pink, 5 = Orange, 6 = Teal
+
+    // Use this for initialization
+    void Start () {
+		// Init for Node - set variables
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
