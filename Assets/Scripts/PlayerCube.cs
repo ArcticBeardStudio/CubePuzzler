b55d9d68b8b03135dcba1aadd1db4a7f48fe6470
Mathias Litzén <matlit-2@student.ltu.se>
2018-01-14 19:06:18 +0100

    player
    Added playmovement

--- Assets/Scripts/PlayerCube.cs
+++ Assets/Scripts/PlayerCube.cs
@@ -7,14 +7,16 @@ public class PlayerCube : MonoBehaviour {
     // Use this for initialization
      public Material PlayerMaterial;
     private int currenttileindex;
+    private Vector3 offset = new Vector3(0, 1, 0);
+    bool playerMoved = false;
 
-     
+
 
 
     void Start () {
         //PlayerMaterial = 
         GetComponent<MeshRenderer>().material = PlayerMaterial;
-        currenttileindex = Manager_script.instance.boardWidth / 2;
+        currenttileindex = 2;
     }
 	
 	// Update is called once per frame
@@ -25,26 +27,45 @@ public class PlayerCube : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (currenttileindex - Manager_script.instance.boardWidth <= maxboardsize)
-                currenttileindex = currenttileindex + Manager_script.instance.boardWidth;
+            {
+                currenttileindex = currenttileindex + 1;
+                playerMoved = true;
+            }
                 
 
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (currenttileindex- Manager_script.instance.boardWidth >= 0 )
-            currenttileindex = currenttileindex - Manager_script.instance.boardWidth;
+            if (currenttileindex - Manager_script.instance.boardWidth >= 0)
+            {
+                currenttileindex = currenttileindex - 1;
+                playerMoved = true;
+            }
 
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (currenttileindex - 1 >= 0)
-                currenttileindex = currenttileindex - 1;
+            {
+                currenttileindex = currenttileindex - Manager_script.instance.boardLength;
+                playerMoved = true;
+            }
+                
             
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (currenttileindex - 1 <= maxboardsize)
-                currenttileindex = currenttileindex + 1;
+            if (currenttileindex + 1 <= maxboardsize)
+            {
+                currenttileindex = currenttileindex + Manager_script.instance.boardLength;
+                playerMoved = true;
+            }
+        }
+        if(playerMoved)
+        {
+            transform.position = Manager_script.instance.Board[currenttileindex].transform.position + offset;
+            Manager_script.instance.PlayerMoved();
+            playerMoved = false;
         }
     }
     void Animate()
